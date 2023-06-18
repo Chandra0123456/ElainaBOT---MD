907a548ab03bdedca70be6cfe7896a64d0af8e34
Chandra0123456 <128498659+Chandra0123456@users.noreply.github.com>
2023-06-18 16:10:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,10 +47,10 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 /*============== SOCIAL ==============*/
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
-global.sig = 'https://instagram.com/Xiao_yan_21' //Link Instagrammu
-global.sgh = 'https://github.com/ImYanXiao' //Link Githubmu
-global.sgc = 'https://chat.whatsapp.com/DyBMXDw1IshFM9z0ZHZNlR'
-global.sdc = '-' //Isi Pake Link Discordmu Kalo Gada Biarin aja
+//global.sig = 'https://instagram.com/Xiao_yan_21' //Link Instagrammu
+//global.sgh = 'https://github.com/ImYanXiao' //Link Githubmu
+//global.sgc = 'https://chat.whatsapp.com/DyBMXDw1IshFM9z0ZHZNlR'
+//global.sdc = '-' //Isi Pake Link Discordmu Kalo Gada Biarin aja
 global.snh = 'https://nhentai.net/g/HaramTod🗿' //Hmmm, Ini boleh kalian isi terserah :v
 
 /*============== PAYMENT ==============*/
